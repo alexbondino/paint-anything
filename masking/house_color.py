c55fcaf6ea5c8848ea3f7e51c58a964d8917deab
Daniel <96345639+alexbondino@users.noreply.github.com>
2023-05-23 12:19:26 -0400

    Update house_color.py
    Corrected

--- masking/house_color.py
+++ masking/house_color.py
@@ -19,21 +19,19 @@ def show_points(coords:np.ndarray, labels:np.ndarray, ax, marker_size=200):
     ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='.', s=marker_size, edgecolor='white', linewidth=1)
     ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='.', s=marker_size, edgecolor='white', linewidth=1)  
 
-## uploading model
-def upload_model(image:np.ndarray):
-    '''Uploads the model obtained from "https://github.com/facebookresearch/segment-anything" and generate the predictor 
-     and masks for the given image
-    * predictor: Process the image to produce an image embedding
-    * masks: list with all posible mask combinations
+## Model Fetching
+def model_fetching():
+    '''Uploads the model obtained from "https://github.com/facebookresearch/segment-anything" and generates the mask_generator
+    and the sam used for the prediction in in model_aplication function.
     
     Note: sam_checkpoint needs to be downloaded in local due to it being too heavy for github to store. 
     Note2: Device can be changed to CUDA for better performance'''
     ## Uploading the model obtained from SAM github
     import sys
     sys.path.append("..")
-    from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
+    from segment_anything import sam_model_registry, SamAutomaticMaskGenerator
 
-    sam_checkpoint = "imagine-houses/sam_vit_h_4b8939.pth"
+    sam_checkpoint = "masking/sam_vit_h_4b8939.pth"
     model_type = "vit_h"
 
     device = "cpu"
@@ -42,14 +40,26 @@ def upload_model(image:np.ndarray):
     sam.to(device=device)
 
     mask_generator = SamAutomaticMaskGenerator(sam)
+    return mask_generator, sam
+    
+## Model Aplication
+def model_aplication(image:np.ndarray, mask_generator, sam):
+    '''Applies the model in Model Fetching and generates the predictor and
+     the masks used for used in the image.
+    
+    Note: sam_checkpoint needs to be downloaded in local due to it being too heavy for github to store. 
+    Note2: Device can be changed to CUDA for better performance'''
+    from segment_anything import SamPredictor
+
     masks = mask_generator.generate(image)
 
     predictor = SamPredictor(sam)
     predictor.set_image(image)
     return predictor, masks
 
+
 ## Generating the final mask
-def masking(input_point: np.ndarray, input_label: np.ndarray, image: np.ndarray, predictor):
+def masking(input_point: np.ndarray, input_label: np.ndarray,  predictor):
     '''Uses the model over some given points to generate a chosen mask given the input data.'''
     ## Using the code with the input point and input label given
     masks, scores, logits = predictor.predict(
@@ -70,15 +80,17 @@ def masking(input_point: np.ndarray, input_label: np.ndarray, image: np.ndarray,
 
 
 if __name__ == "__main__":
-    image = cv2.imread('imagine-houses/masking/samples/casa.jpeg')
+    
+    image = cv2.imread('masking/samples/casa.jpeg')
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    predictor_test, masks_test = upload_model(image)
+    mask_generator_test, sam_test = model_fetching()
+    predictor_test, masks_test = model_aplication(image, mask_generator_test, sam_test)
 
     input_point = np.array([[50,65],[50,100],[123,50]])
     input_label = np.array([1,0,1])
 
-    masks_test = masking(input_point, input_label, image, predictor_test)
+    masks_test = masking(input_point, input_label, predictor_test)
 
     plt.imshow(image)
     show_mask(masks_test, plt.gca())
