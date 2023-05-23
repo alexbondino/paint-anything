22e85aa20d7673915724fbf10ed591a9fb9ddd29
Daniel <96345639+alexbondino@users.noreply.github.com>
2023-05-23 13:36:43 -0400

    Upload-image finished
    Finished feature, ready for PR

---
+++ web-page/backend/env/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/image', methods=['POST'])
+def post_image():
+    image = request.files.get("imagen")
+    image.save('image output/image.jpg')
+    return jsonify({'message': 'Archivo guardado correctamente.'})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
---
+++ web-page/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ImageUploader from './components/upload_image/upload_image.js';
+
+const App = () => {
+  return (
+    <div>
+      <ImageUploader/>
+    </div>
+  );
+};
+
+export default App;
