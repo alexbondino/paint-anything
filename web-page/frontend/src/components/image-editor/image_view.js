d4770eaf6f0d8304ad70694610f526c467aee914
Loreto <loretopalmad@gmail.com>
2023-06-05 23:46:33 -0400

    fix: indicate purpose of ImageView

--- web-page/frontend/src/components/image-editor/image_view.js
+++ web-page/frontend/src/components/image-editor/image_view.js
@@ -1,6 +1,8 @@
 //import React, { useEffect } from 'react';
 
+//TODO: Component for future view of image and layers.
 function ImageView(image) {
+    
     return(
         <div>
             <img src={image.path} alt="uploaded"/>
