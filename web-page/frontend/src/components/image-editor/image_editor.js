27c9a5e4404e644dc7fd3f6de8ab18d99c4e5d89
Daniel <96345639+alexbondino@users.noreply.github.com>
2023-07-02 16:45:03 -0400

    True Points ready

--- web-page/frontend/src/components/image-editor/image_editor.js
+++ web-page/frontend/src/components/image-editor/image_editor.js
@@ -3,6 +3,9 @@ import Box from '@mui/material/Box';
 import './image-editor.scss';
 import axios from 'axios';
 
+
+
+
 /*
  * Image editor
  */
@@ -16,12 +19,25 @@ export default function ImageEditor({
   // construct mask images dynamically from layer definitions
   const [coordinateX, setCoordinateX] = useState(0)
   const [coordinateY, setCoordinateY] = useState(0)
-  
+  const [truePoints, setTruePoints] = useState([]);
 
   useEffect(() => {
     console.log('Coordenadas:', coordinateX, coordinateY);
   
   }, [coordinateX, coordinateY]);
+
+  useEffect(() => {
+    const newLayerDef = [...layersDef];
+    const layerPos = newLayerDef.findIndex((l) => l.id === selectedLayer);
+    if (layerPos !== -1) {
+      setTruePoints(layersDef[layerPos].layerTrueCoords);
+    }
+    else {
+      setTruePoints([{}])
+    }
+  }, [selectedLayer]);
+
+
 
   const handlePointAndClick = async (event) => {
     // Obtain the true image coords
@@ -35,14 +51,9 @@ export default function ImageEditor({
     const containerX = clientX - boxRect.left;
     const containerY = clientY - boxRect.top;
   
-    // Obtain the image width and height
-    const imgElement = document.querySelector('.image-box img');
-    const imageWidth = imgElement.naturalWidth;
-    const imageHeight = imgElement.naturalHeight;
-  
     // Calculate relative image coordinates
-    const imageX = (containerX / boxRect.width) * imageWidth;
-    const imageY = (containerY / boxRect.height) * imageHeight;
+    const imageX = containerX;
+    const imageY = containerY;
   
     setCoordinateX(imageX);
     setCoordinateY(imageY);
@@ -61,6 +72,8 @@ export default function ImageEditor({
       } catch (error){
         console.error('Error al enviar coordenadas positivas:', error);
       }
+      setTruePoints(layersDef[layerPos].layerTrueCoords)
+
     } else if (event.type === 'contextmenu' && layerPos!==-1) {
       event.preventDefault();
   
@@ -80,6 +93,7 @@ export default function ImageEditor({
     }
   };
 
+  
 
   const maskImgComps = layersDef
     .filter((l) => l.imgUrl !== null)
@@ -108,7 +122,23 @@ export default function ImageEditor({
   return (
     <Box className="background-full" sx={{ display: sidebarVisibility, flexDirection: 'column' }}>
       <Box className="image-box" sx={{ position: 'relative' }}>
-        <img src={baseImg} alt="base_image" onClick={handlePointAndClick} onContextMenu={handlePointAndClick}/>
+        <img src={baseImg} className="image" alt="base_image" onClick={handlePointAndClick} onContextMenu={handlePointAndClick}/>
+        {truePoints.length > 0 && truePoints[0][1] && truePoints[0][0] && truePoints.map((truePoint, index) => (
+          <Box
+            key={index}
+            sx={{
+              position: 'absolute',
+              top: truePoint[1] - 5,
+              left: truePoint[0] - 5,
+              width: '10px',
+              height: '10px',
+              backgroundColor: 'red',
+              borderRadius: '50%',
+              backgroundColor: 'green',
+              border: '1px solid white',
+            }}
+          />
+        ))}
         {maskImgComps}
       </Box>
     </Box>
