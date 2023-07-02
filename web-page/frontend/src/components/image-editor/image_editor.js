7d1d5eb50acee41950b55f9934352e4ab3f7220a
Javier <javier.smith.dlc@gmail.com>
2023-07-02 01:03:54 -0400

    fix: point and click when there are masks loaded

--- web-page/frontend/src/components/image-editor/image_editor.js
+++ web-page/frontend/src/components/image-editor/image_editor.js
@@ -3,6 +3,63 @@ import Box from '@mui/material/Box';
 import './image-editor.scss';
 import axios from 'axios';
 
+/**
+ * Extracts base image size. Don't call this unless image is loaded
+ * @returns [width, height]
+ */
+function getBaseImageSize() {
+  // Obtain the image width and height
+  const imgElement = document.querySelector('.image-box img');
+  const imageWidth = imgElement.naturalWidth;
+  const imageHeight = imgElement.naturalHeight;
+  return [imageWidth, imageHeight];
+}
+
+/**
+ * Renders mask images, returning an array of html images
+ * @param {Array} layersDef array with layer definitions
+ * @param {int} selectedLayer id of selected layer
+ * @param {Array} imgSize [width, height] for image rendering
+ * @param {function} onPointAndClick trigger for when image is selected and click is performed
+ * @returns
+ */
+const MaskImages = ({ layersDef, selectedLayer, imgSize, onPointAndClick }) => {
+  return layersDef.map((layer) => {
+    try {
+      return (
+        <img
+          key={layer.id}
+          // If image file has not been defined, loads full transparent image
+          src={
+            layer.imgUrl !== null
+              ? layer.imgUrl
+              : 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
+          }
+          alt={`mask_image_${layer.id}`}
+          width={imgSize[0]}
+          height={imgSize[1]}
+          style={{
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            visibility: layer.visibility ? 'visible' : 'hidden',
+            // if image is selected, this highlights it
+            filter:
+              layer.id === selectedLayer && layer.imgUrl !== null
+                ? 'drop-shadow(1px 1px 0 yellow) drop-shadow(-1px -1px 0 yellow) drop-shadow(1px -1px 0 yellow) drop-shadow(-1px 1px 0 yellow)'
+                : 'none',
+          }}
+          onClick={onPointAndClick}
+          onContextMenu={onPointAndClick}
+        />
+      );
+    } catch {
+      console.log(`Image for layer ${layer.id} not found`);
+      return;
+    }
+  });
+};
+
 /*
  * Image editor
  */
@@ -16,10 +73,14 @@ export default function ImageEditor({
   // construct mask images dynamically from layer definitions
   const [coordinateX, setCoordinateX] = useState(0);
   const [coordinateY, setCoordinateY] = useState(0);
+  const [baseImgSize, setBaseImgSize] = useState([]);
 
-  useEffect(() => {
-    console.log('Coordenadas:', coordinateX, coordinateY);
-  }, [coordinateX, coordinateY]);
+  useEffect(() => {}, [coordinateX, coordinateY]);
+
+  const handleOnBaseImageLoad = () => {
+    const newImageSize = getBaseImageSize();
+    setBaseImgSize(newImageSize);
+  };
 
   const handlePointAndClick = async (event) => {
     // Obtain the true image coords
@@ -33,14 +94,9 @@ export default function ImageEditor({
     const containerX = clientX - boxRect.left;
     const containerY = clientY - boxRect.top;
 
-    // Obtain the image width and height
-    const imgElement = document.querySelector('.image-box img');
-    const imageWidth = imgElement.naturalWidth;
-    const imageHeight = imgElement.naturalHeight;
-
     // Calculate relative image coordinates
-    const imageX = (containerX / boxRect.width) * imageWidth;
-    const imageY = (containerY / boxRect.height) * imageHeight;
+    const imageX = (containerX / boxRect.width) * baseImgSize[0];
+    const imageY = (containerY / boxRect.height) * baseImgSize[1];
 
     setCoordinateX(imageX);
     setCoordinateY(imageY);
@@ -76,42 +132,18 @@ export default function ImageEditor({
     }
   };
 
-  const maskImgComps = layersDef
-    .filter((l) => l.imgUrl !== null)
-    .map((layer) => {
-      try {
-        return (
-          <img
-            key={layer.id}
-            src={layer.imgUrl}
-            alt={`mask_image_${layer.id}`}
-            style={{
-              position: 'absolute',
-              width: '100%',
-              height: '100%',
-              visibility: layer.visibility ? 'visible' : 'hidden',
-              filter:
-                layer.id === selectedLayer
-                  ? 'drop-shadow(1px 1px 0 yellow) drop-shadow(-1px -1px 0 yellow) drop-shadow(1px -1px 0 yellow) drop-shadow(-1px 1px 0 yellow)'
-                  : 'none',
-            }}
-          />
-        );
-      } catch {
-        console.log(`Image for layer ${layer.id} not found`);
-        return;
-      }
-    });
   return (
     <Box className="background-full" sx={{ display: sidebarVisibility, flexDirection: 'column' }}>
       <Box className="image-box" sx={{ position: 'relative' }}>
-        <img
-          src={baseImg}
-          alt="base_image"
-          onClick={handlePointAndClick}
-          onContextMenu={handlePointAndClick}
-        />
-        {maskImgComps}
+        <img src={baseImg} alt="base_image" onLoad={handleOnBaseImageLoad} />
+        {baseImgSize.length === 2 ? (
+          <MaskImages
+            layersDef={layersDef}
+            selectedLayer={selectedLayer}
+            imgSize={baseImgSize}
+            onPointAndClick={handlePointAndClick}
+          />
+        ) : null}
       </Box>
     </Box>
   );
