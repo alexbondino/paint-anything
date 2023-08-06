087d6c720dfba7ab22365dd9091808a744367894
Javier <javier.smith.dlc@gmail.com>
2023-08-05 21:14:49 -0400

    chore: moved hsl slider to separate component

---
+++ web-page/frontend/src/components/side_nav/HSLSlider.js
@@ -0,0 +1,118 @@
+import { useEffect, useState } from 'react';
+import { Box, Slider, Typography } from '@mui/material';
+
+function valueLabelFormat(value) {
+  return `${value} ${'%'}`;
+}
+
+/**
+ * Hue, saturation and lightness sliders
+ * @param {int} layerId
+ * @param {int} hue
+ * @param {int} saturation
+ * @param {int} lightness
+ * @param {function} onHSLChange
+ * @returns HSL slider for modifying hsl color of layer
+ */
+const HSLSlider = ({ layerId, hue, saturation, lightness, onHSLChange }) => {
+  const [isDragging, setIsDragging] = useState(false);
+  const [sliderPoint, setSliderPoint] = useState(50);
+  const [newLightness, setNewLightness] = useState(lightness);
+  const [now, setNow] = useState(new Date().getTime());
+
+  const handleSliderDragStart = () => {
+    setIsDragging(true);
+    setNow(new Date().getTime());
+  };
+
+  const handleSliderDragEnd = (newValue) => {
+    setIsDragging(false);
+    const newLightness = Math.min(100, Math.max(0, newValue));
+    setSliderPoint(newLightness);
+  };
+
+  const handleOnHSLChange = (newValue) => {
+    setIsDragging(true);
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (isDragging) {
+        let adjustedLightness = newLightness + (sliderPoint - 50) / 50;
+        adjustedLightness = Math.min(100, Math.max(0, adjustedLightness));
+        setNewLightness(adjustedLightness);
+
+        console.log(sliderPoint - 50);
+
+        console.log('now: ', now);
+        console.log('new lightness: ', newLightness);
+        console.log('lightness:', lightness);
+
+        onHSLChange([hue, saturation, newLightness], layerId);
+      }
+    }, 10);
+    return () => clearInterval(timer);
+  }, [newLightness, layerId, hue, saturation, onHSLChange, sliderPoint]);
+
+  const sliderOnChange = (newValue) => {
+    setSliderPoint(newValue);
+    console.log('slider on change: ', sliderPoint);
+  };
+
+  return (
+    <Box className="sliders-box" sx={{ draggable: false }}>
+      <Typography variant="button" id="input-slider" gutterBottom>
+        Hue
+      </Typography>
+      <Slider
+        id="rHue"
+        aria-label="Hue"
+        size="small"
+        value={hue}
+        min={0}
+        max={360}
+        valueLabelDisplay="auto"
+        onChange={(e) => onHSLChange([e.target.value, saturation, lightness], layerId)}
+      ></Slider>
+      <Typography variant="button" id="input-slider" gutterBottom>
+        Saturation
+      </Typography>
+      <Slider
+        id="rSat"
+        aria-label="Saturation"
+        value={saturation}
+        size="small"
+        min={0}
+        max={100}
+        valueLabelDisplay="auto"
+        valueLabelFormat={valueLabelFormat}
+        onChange={(e) => onHSLChange([hue, e.target.value, lightness], layerId)}
+      ></Slider>
+      <Typography variant="button" id="input-slider" gutterBottom>
+        Lightness
+      </Typography>
+      <Slider
+        id="rL"
+        aria-label="Lightness"
+        size="small"
+        min={0}
+        max={100}
+        color="info"
+        value={sliderPoint}
+        valueLabelDisplay="auto"
+        valueLabelFormat={valueLabelFormat((sliderPoint - 50) / 10)}
+        onMouseDown={() => handleOnHSLChange(sliderPoint)}
+        onChange={(e) => sliderOnChange(e.target.value)}
+        onMouseUp={() => {
+          handleSliderDragEnd(50);
+        }}
+        onDragStart={handleSliderDragStart}
+        onMouseLeave={() => {
+          handleSliderDragEnd(50);
+        }}
+      ></Slider>
+    </Box>
+  );
+};
+
+export default HSLSlider;
--- web-page/frontend/src/helpers.js
+++ web-page/frontend/src/helpers.js
@@ -5,3 +5,22 @@ export const reorder = (list, startIndex, endIndex) => {
   return result;
 };
 
+/**
+ *  Converts hsl color model to hexadecimal string representation
+ * @param {int} h hue value [0-360]
+ * @param {int} s saturation value [0-100]
+ * @param {int} l lightness value [0-100]
+ * @returns hexadecimal string rep
+ */
+export function hslToHex(h, s, l) {
+  l /= 100;
+  const a = (s * Math.min(l, 1 - l)) / 100;
+  const f = (n) => {
+    const k = (n + h / 30) % 12;
+    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+    return Math.round(255 * color)
+      .toString(16)
+      .padStart(2, '0'); // convert to Hex and prefix "0" if needed
+  };
+  return `#${f(0)}${f(8)}${f(4)}`;
+}
